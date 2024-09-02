1b955ca8f0e252c2c2f872a24a325dbf5d11b75a
Karolina Balszan <kbalszan@gmail.com>
2024-09-02 10:06:59 +0100

    Added stripe integration and basic structure to display a list of products

---
+++ app/ProductCard.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function ProductCard({ product }) {
+  return (
+    <div>
+      <p>Product title</p>
+      <p>2</p>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main className="p-4">Lets check</main>
+import Stripe from "stripe"
+import ProductCard from "./ProductCard"
+
+async function getStripeProducts() {
+  const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+    apiVersion: "2024-06-20",
+  })
+  const res = await stripe.prices.list({
+    expand: ["data.product"],
+  })
+  const prices = res.data
+  return prices
+}
+export default async function Home() {
+  const products = await getStripeProducts()
+  return (
+    <main className="p-4 flex flex-col">
+      <div className="max-w-[1000px] w-full mx-aut grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        {products.map((product, productIndex) => {
+          return <ProductCard key={productIndex} product={product} />
+        })}
+      </div>
+    </main>
+  )
 }
